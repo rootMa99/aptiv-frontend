33f60c9ab5e21cf55a257648786cc0e6385b0b6b
admin <anasszeroual09@gmail.com>
2023-11-15 17:09:07 +0100

    io

---
+++ src/component/upload/ProgressBarComp.js
@@ -0,0 +1,7 @@
+import "axios-progress-bar/dist/nprogress.css";
+//import ProgressBar from "axios-progress-bar";
+
+const ProgressBarComp = (p) => {
+  //return <ProgressBar />;
+};
+export default ProgressBarComp;
--- src/component/upload/UploadFiles.js
+++ src/component/upload/UploadFiles.js
@@ -1,31 +1,62 @@
 import React, { useState } from "react";
 import c from "./UploadFiles.module.css";
-import LoadingFetch from "../UI/LoadingFetch";
+import axios from "axios";
 
-const postData = async (url, body) => {
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      body: body,
-    });
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {}
-};
+// const postData = async (url, body) => {
+//   try {
+//     const response = await fetch(url, {
+//       method: "POST",
+//       body: body,
+//     });
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok");
+//     }
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {}
+// };
 
 const UploadFiles = (p) => {
   const [datafile, setDatafile] = useState(null);
   const [datafilef, setDatafilef] = useState(null);
-  const [dataret, setDataret]= useState([]);
+  const [dataret, setDataret] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
+
 
 
   const changeHandler = (e) => {
     setDatafile(e.target.files[0]);
   };
+
+  console.log(progress);
+  const sendingData= async(url,formData)=>{
+    try {
+      setIsLoading(true)
+      // Start the progress bar
+      setProgress(0);
+
+      const response = await axios.post(
+        url,
+        formData,
+        {
+          onUploadProgress:  (progressEvent) => {
+            const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+            console.log(percentCompleted);
+            setProgress(percentCompleted);
+          },
+        }
+      );
+      console.log(response);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Error sending data:", error);
+    } finally {
+      
+      setIsLoading(false)
+    }
+  }
 
   console.log(datafile);
   const submmitHandler = async (e) => {
@@ -34,20 +65,23 @@ const UploadFiles = (p) => {
       const formData = new FormData();
       formData.append("file", datafile);
       console.log("start sending...");
-      setIsLoading(true);
-      const data = await postData(
-        "http://localhost:8081/personel/personel",
-        formData
-      );
-      setIsLoading(false);
+      // setIsLoading(true);
+      // const data = await postData(
+      //   "http://localhost:8081/personel/personel",
+      //   formData
+      // );
+      // setIsLoading(false);
+
+     await sendingData("http://localhost:8081/personel/personel", formData);
+
       console.log("data fetched");
-      console.log(data);
+      setProgress(100);
     }
   };
 
-  const changeHandlerFormation=e=>{
+  const changeHandlerFormation = (e) => {
     setDatafilef(e.target.files[0]);
-  }
+  };
   console.log(datafile);
   const submmitHandlerFormation = async (e) => {
     e.preventDefault();
@@ -55,37 +89,40 @@ const UploadFiles = (p) => {
       const formData = new FormData();
       formData.append("file", datafilef);
       console.log("start sending...");
-      setIsLoading(true);
-      const data = await postData(
-        "http://localhost:8081/formation/uploadFormation",
-        formData
-      );
-      setDataret(data)
-      setIsLoading(false);
+      // setIsLoading(true);
+      // const data = await postData(
+      //   "http://localhost:8081/formation/uploadFormation",
+      //   formData
+      // );
+      // setIsLoading(false);
+      //setDataret(data);
+      const data= await sendingData("http://localhost:8081/formation/uploadFormation", formData);
+      console.log(data);
+      setDataret(data);
       console.log("data fetched");
+      setProgress(100);
     }
-};
-console.log(dataret);
+  };
+  console.log(dataret);
   console.log(datafilef);
   return (
     <main className={c.main}>
       <div className={c.inputscontainer}>
-        {
-            dataret.length>0 && (
-                <React.Fragment>
-                <ul className={c.unlist}>
-                <h3>Employees not Found</h3>
-                   { dataret.map((m)=>
-                    <li key={m.formationId} className={c.listItem}>
-                        <span>{m.matricule}</span>
-                        <span>{m.type}</span>
-                        <span>{m.categorieFormation}</span>
-                        <span>{m.formatteur}</span>
-                    </li>)}
-                </ul>
-                </React.Fragment>
-            )
-        }
+        {dataret.length > 0 && (
+          <React.Fragment>
+            <ul className={c.unlist}>
+              <h3>Employees not Found</h3>
+              {dataret.map((m) => (
+                <li key={m.formationId} className={c.listItem}>
+                  <span>{m.matricule}</span>
+                  <span>{m.type}</span>
+                  <span>{m.categorieFormation}</span>
+                  <span>{m.formatteur}</span>
+                </li>
+              ))}
+            </ul>
+          </React.Fragment>
+        )}
         <form onSubmit={submmitHandler} className={c.form}>
           <label htmlFor="arquivo" className={c.label}>
             upload Employee data to data base:
@@ -99,9 +136,19 @@ console.log(dataret);
             onChange={changeHandler}
           />
           <button className={c["ui-btn"]}>
-             {!isLoading ?<span> upload </span>:<LoadingFetch />}
+            <span>{!isLoading ? "upload" : "uploading..."}</span>
           </button>
         </form>
+        {isLoading && (
+          <div className={c["progress-bar-container"]}>
+            <div
+              className={c["progress-bar"]}
+              style={{ width: `${progress}%` }}
+            >
+              {progress}%
+            </div>
+          </div>
+        )}
         <form onSubmit={submmitHandlerFormation} className={c.form}>
           <label htmlFor="arquivo" className={c.label}>
             upload formation data to data base:
@@ -115,7 +162,7 @@ console.log(dataret);
             onChange={changeHandlerFormation}
           />
           <button className={c["ui-btn"]}>
-             {!isLoading ?<span> upload</span> : <LoadingFetch /> }
+          <span>{!isLoading ?  "upload" : "uploading..."}</span>
           </button>
         </form>
       </div>
